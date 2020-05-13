1cb3bde9188e4bb32286c484cf4e10f969442dfe
Wim Fournier <wifournier@ebay.com>
2020-05-13 22:35:22 +0200

    added wrapper for ina260 lib

---
+++ sensors/PowerSensor.py
@@ -0,0 +1,17 @@
+import adafruit_ina260
+import board
+
+
+class INA260Sensor:
+    """
+    INA260 voltage, current and power sensor
+    """
+
+    def __init__(self):
+        i2c = board.I2C()
+        self.ina260 = adafruit_ina260.INA260(i2c)
+        self.ina260.mode = adafruit_ina260.Mode.CONTINUOUS
+        self.ina260.averaging_count =  adafruit_ina260.AveragingCount.COUNT_4
+
+    def read(self):
+        return {"power": self.ina260.power, "current": self.ina260.current, "voltage": self.ina260.voltage}
