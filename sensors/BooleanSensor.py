a70d58473a10da2158df0cacd1758aa54025cac4
Wim Fournier <wifournier@ebay.com>
2020-05-13 23:00:06 +0200

    rename

--- sensors/BooleanSensor.py
+++ sensors/BooleanSensor.py
@@ -1,11 +1,11 @@
 class BooleanSensor:
-    def __init__(self, gpio: int):
-        self.gpio = gpio
-        GPIO.setup(gpio, GPIO.IN)
+    def __init__(self, gpio_pin: int):
+        self.gpio_pin = gpio_pin
+        GPIO.setup(gpio_pin, GPIO.IN)
 
     def read(self) -> bool:
         """
         reads from the sensor and returns the state
         :return: bool
         """
-        return GPIO.input(self.gpio) != 0
+        return GPIO.input(self.gpio_pin) != 0
