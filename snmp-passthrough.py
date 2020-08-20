27e5e3cda0a4593bb3ef0a63d055488ca6f8424d
Wim Fournier <wifournier@ebay.com>
2020-08-20 20:00:33 +0200

    add missing sensor type

--- snmp-passthrough.py
+++ snmp-passthrough.py
@@ -18,7 +18,7 @@ def update():
             PP.add_int("0.{}".format(index), 1 if sensor.get("value") else 0, sensor.get("name"))
             PP.add_str("1.{}".format(index), sensor.get("name"))
             continue
-        if sensor.get("type") in ["rpm", "temperature", "flow"]:
+        if sensor.get("type") in ["rpm", "temperature", "flow", "pa_power"]:
             PP.add_int("0.{}".format(index), sensor.get("value", -128), sensor.get("name"))
             PP.add_str("1.{}".format(index), sensor.get("name"))
             continue
